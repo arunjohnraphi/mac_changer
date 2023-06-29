0b6b972e7e81681b6ef9355485c7147180311b4c
arunjohnraphi <arunjohnraphi@gmail.com>
2023-06-29 17:14:06 +0530

    default interface added

--- mac_changer.py
+++ mac_changer.py
@@ -1,7 +1,15 @@
 import subprocess
+from colorama import Fore ,Style
 
+default = "wlp2s0"
 
-interface = input("interface > hint enp1s0f1 >")
+print ("[+] Default interface :"+ default)
+print(Fore.RED + "press Enter to use the default"+Style.RESET_ALL)
+interface = input("interface > ")
+
+if interface == '' :
+    interface = default
+
 new_mac = input("New MAC Adress : >")
 
 print("[+] Changing MAC Adress of " + interface + " to " + new_mac)
@@ -11,3 +19,5 @@ subprocess.call("ifconfig " + interface + " hw ether " + new_mac ,shell=True)
 subprocess.call("ifconfig " + interface + " up",shell=True)
 subprocess.call("ifconfig | grep ether",shell=True)
 
+if __name__ == "__main__" :
+    pass
